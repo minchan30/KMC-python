57ebae61fa7d8d5b0ea19daede47cd6d4ce180fb
cmworld06 <cmworld06@nate.com>
2023-06-11 12:23:37 +0900

    :memo:update

---
+++ MInchan/input output (io)/심화문제 2장 S2-1.py
@@ -0,0 +1,10 @@
+a = input('변환할 킬로그램(kg)은? ')
+b = ('-' * 50)
+c = int(a) * 2.204623
+d = int(a) * 35.273962
+e = '킬로그램        파운드         온스'
+print(b)
+print(e)
+print(b)
+print('%s          %.2f      %.2f' %(a,c,d))
+print(b)
