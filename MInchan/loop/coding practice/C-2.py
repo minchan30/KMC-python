ac225ffd8efae79acd63d789882db1889d9b85f8
minchan30 <goalie0901@gmail.com>
2023-07-08 12:25:53 +0900

    :tada:update

--- MInchan/loop/coding practice/C-2.py
+++ MInchan/loop/coding practice/C-2.py
@@ -6,4 +6,6 @@ for cm in range(1 , 101) :
     mm = cm * 10.0
     m = cm * 0.01
     inch = cm * 0.3937
-    print('%d , %d , %d , %.2f')
+    print('%d %d %.2f %.1f' %(cm , mm , m , inch))
+
+print('-' * 40)
